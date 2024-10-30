bf06fd2063366b0147ce77d8b53f4e6e74861d71
rkodiyalam636 <rkodiyal@purdue.edu>
2024-10-29 20:07:27 -0400

    Create SocialMediaUser.java

---
+++ SocialMediaUser.java
@@ -0,0 +1,9 @@
+
+public interface SocialMediaUser {
+	public String getName();
+	public void setName(String name);
+	public String getUsername();
+	public void setUsername(String username);
+	public String getPassword();
+	public void setPassword(String password);
+}
