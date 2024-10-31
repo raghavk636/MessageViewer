3be39bd668f4ca1ae4305295aaba3bba9663939d
Aditya Agrawal <agraw306@purdue.edu>
2024-10-31 13:20:54 -0400

    Create Message.java

---
+++ Message.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class Message {
+
+    // fields
+
+    String content = ""; //content of the message
+    MessageViewerUser sender; //user that sends the message
+    MessageViewerUser receiver; //user that recieves the message
+
+    // constructors
+
+
+    public Message(String content, MessageViewerUser sender, MessageViewerUser receiver) {
+
+        //init
+        this.content = content;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public Message(String content, String username) {
+        this.content = content;
+
+    }
+
+    //getter methods for Message objects
+
+    //gets content of a message
+    public String getContent() {
+        return content;
+    }
+
+    //gets the sender of a message
+    public MessageViewerUser getSender() {
+        return sender;
+    }
+
+    //gets the reciever of a message
+    public MessageViewerUser getReceiver() {
+        return receiver;
+    }
+
+    //setter methods
+
+    //sets the content of a message
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    //sets the sender of a message
+    public void setSender(MessageViewerUser sender) {
+        this.sender = sender;
+    }
+
+    //sets the reciever of a message
+    public void setReceiver(MessageViewerUser receiver) {
+        this.receiver = receiver;
+    }
+
+}
