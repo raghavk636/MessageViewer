595336461c99fac8f53d4cb7968f0c137a28ca8b
Aditya Agrawal <agraw306@purdue.edu>
2024-11-03 16:41:29 -0500

    InvalidPasswordException (without javadoc)

--- InvalidPasswordException.java
+++ InvalidPasswordException.java
@@ -1,6 +1,6 @@
 
 public class InvalidPasswordException extends Exception{
-	public InvalidPasswordException(String message) {
-		super(message);
-	}
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
 }
