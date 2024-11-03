5278f5371b004f5f8d25b2cc61c1870484a07012
Aditya Agrawal <agraw306@purdue.edu>
2024-11-03 16:30:07 -0500

    Message Interface(without javadoc)

--- MessageInterface.java
+++ MessageInterface.java
@@ -1,11 +1,11 @@
 public interface MessageInterface {
-    
-    //METHODS 
-    
+
+    //METHODS
+
     String getContent();
     void setContent(String content);
     MessageViewerUser getSender();
     void setSender(MessageViewerUser sender);
     MessageViewerUser getReceiver();
     void setReceiver(MessageViewerUser receiver);
-}
+} 
