305742d7c0205035b0cb3e829b4f1c37ec458c8e
Aditya Agrawal <agraw306@purdue.edu>
2024-11-03 20:20:54 -0500

    Update InvalidUsernameException.java javadoc

--- InvalidUsernameException.java
+++ InvalidUsernameException.java
@@ -1,4 +1,9 @@
-
+/**
+ * InvalidUsernameException - A custom exception thrown when an invalid username is provided during user registration
+ *
+ * @author L-10 Team-1 
+ * @version November 3, 2024
+ */
 public class InvalidUsernameException extends Exception{
     public InvalidUsernameException(String message) {
         super(message);
