1f57d3a5f7faa7fce3e0fecb91c96ae2f7c02a41
Aditya Agrawal <agraw306@purdue.edu>
2024-11-03 20:18:39 -0500

    Update InvalidPasswordException.java javadoc

--- InvalidPasswordException.java
+++ InvalidPasswordException.java
@@ -1,4 +1,9 @@
-
+/**
+ * InvalidPasswordException - A custom exception thrown when an invalid password is provided during a user authentication attempt.
+ *
+ * @see Exception
+ * @version November 3, 2024
+ */
 public class InvalidPasswordException extends Exception{
     public InvalidPasswordException(String message) {
         super(message);
