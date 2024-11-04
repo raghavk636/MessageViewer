7c113617f9c105b9d26f666a023a25f3f8629a0f
Aditya Agrawal <agraw306@purdue.edu>
2024-11-03 20:17:06 -0500

    Update BlockedUserException.java (with javadoc)

--- BlockedUserException.java
+++ BlockedUserException.java
@@ -1,4 +1,9 @@
-
+/**
+ * BlockedUserException - A custom exception thrown when an action is attempted on a user who is blocked.
+ *  
+ * @author L10-Team 1
+ * @version November 3, 2024
+ */
 public class BlockedUserException extends Exception{
     public BlockedUserException(String message) {
         super(message);
