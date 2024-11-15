a733c38ec141b4c6a6ba133f82de8bf18a3af83d
Wedget0 <sing1614@purdue.edu>
2024-11-14 22:18:23 -0500

    Update ClientInterface.java
    added javadoc

--- ClientInterface.java
+++ ClientInterface.java
@@ -1,4 +1,11 @@
+/**
+ * ClientInterface - An interface defining methods for the start screen and home screen that will be viewed by the user.
+ * 
+ * @author L10-Team 1 
+ * @version November 14, 2024
+ */
 public interface ClientInterface {
+    // METHODS
     static int startScreen() {
         return 0;
     }
