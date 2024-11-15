3c6153b767c8521a72c1492f451c5554eb45a939
Wedget0 <sing1614@purdue.edu>
2024-11-14 22:31:49 -0500

    Update ServerInterface.java
    javadoc

--- ServerInterface.java
+++ ServerInterface.java
@@ -1,7 +1,14 @@
+/**
+ * ServerInterface - An interface defining the methods used in the server class.
+ *
+ * 
+ * @author L10-Team 1
+ * @version November 14, 2024
+ */
 import java.util.ArrayList;
 
 public interface ServerInterface {
-
+// METHODS
     static void saveUsersToFile() // Save users to file
     {}
 
