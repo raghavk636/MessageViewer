eb9bff654afd07af1d05c348bc3563482f137b52
rnimbkar <rnimbkar@purdue.edu>
2024-11-16 18:09:12 -0500

    Update ClientGUI.java

--- ClientGUI.java
+++ ClientGUI.java
@@ -1,3 +1,25 @@
+/**
+ * The ClientGUI class provides a console-based interface for interacting with a 
+ * messaging server. Users can log in, create accounts, and perform various actions 
+ * such as searching messages, managing friends, and signing out. 
+ *
+ * This class is part of a client-server messaging application and is intended to 
+ * send user inputs to the server for processing. The implementation includes menus 
+ * for navigation and placeholders for server communication.
+ *
+ * Features include:
+ * <ul>
+ *   <li>Log In functionality</li>
+ *   <li>Account creation</li>
+ *   <li>Friend management (add, remove, block)</li>
+ *   <li>Message searching</li>
+ *   <li>Sign out functionality</li>
+ * </ul>
+ * </p>
+ *
+ * @author
+ * @version 1.0
+ */
 import java.util.*;
 public class ClientGUI implements ClientInterface {
 	
