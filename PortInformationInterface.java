50bec2dafc2f8b3b9c6ce45e3593c05460cc8942
rkodiyalam636 <rkodiyal@purdue.edu>
2024-11-17 16:54:26 -0500

    Create PortInformationInterface.java

---
+++ PortInformationInterface.java
@@ -0,0 +1,5 @@
+
+public interface PortInformationInterface {
+	public static final String SERVER_ADDRESS = "localhost"; // Server address
+    public static final int SERVER_PORT = 12345;			 // Server port
+}
