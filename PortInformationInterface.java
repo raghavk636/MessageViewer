bfd9dcdd04000b2d29428df980902771a1a32f87
rnimbkar <rnimbkar@purdue.edu>
2024-11-17 19:23:52 -0500

    Update PortInformationInterface.java

--- PortInformationInterface.java
+++ PortInformationInterface.java
@@ -1,4 +1,15 @@
-
+/**
+ * The PortInformationInterface defines constants for the server's connection details,
+ * including the address and port number.
+ * 
+ * These constants are used by clients and servers to establish a socket connection.
+ * 
+ * Usage of this interface ensures consistency in the connection details across different
+ * components of the application.
+ * 
+ * @author L10- Team 1
+ * @version November 17 2024
+ */
 public interface PortInformationInterface {
 	public static final String SERVER_ADDRESS = "localhost"; // Server address
     public static final int SERVER_PORT = 12345;			 // Server port
